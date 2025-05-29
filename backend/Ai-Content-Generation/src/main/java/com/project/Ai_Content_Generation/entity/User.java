c81ed96dff2255a746ec7f95a50bcabd8682fd64
Akshay alave <akshayalave@gmail.com>
2025-05-29 10:52:02 +0530

    Switched MySql databse with Supabse(postgres) virtul Database

--- backend/Ai-Content-Generation/src/main/java/com/project/Ai_Content_Generation/entity/User.java
+++ backend/Ai-Content-Generation/src/main/java/com/project/Ai_Content_Generation/entity/User.java
@@ -7,16 +7,21 @@ import java.util.Set;
 
 @Entity
 @Data
+@Table(name = "users") 
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
+    
     @Column(unique = true)
     private String email;
+    
     private String password;
 
     @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
     private Set<String> roles;
 }
-
